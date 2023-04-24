f4ab1b4151e3cfdbe8ec9a88a1c1b0881c2428d0
zhzfight <472412442@qq.com>
2023-04-24 04:07:14 -0700

    first commit

--- param_parser.py
+++ param_parser.py
@@ -17,7 +17,7 @@ def parameter_parser():
                         help='Random seed')
     parser.add_argument('--cpus',
                         type=int,
-                        default=4,
+                        default=12,
                         help='num of cpu')
     parser.add_argument('--device',
                         type=str,
