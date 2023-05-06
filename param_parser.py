5bc087df97c74a5198b02a039dfec38ff6342649
zhzfight <472412442@qq.com>
2023-05-05 23:14:25 -0700

    first commit

--- param_parser.py
+++ param_parser.py
@@ -96,7 +96,7 @@ def parameter_parser():
     # Training hyper-parameters
     parser.add_argument('--batch',
                         type=int,
-                        default=10,
+                        default=20,
                         help='Batch size.')
     parser.add_argument('--epochs',
                         type=int,
